f0f378ae39297855e3b835effcf62f03dd37274d
Sabasiva06 <sabasiva06@gmail.com>
2021-07-11 19:16:42 +0530

    B works

--- src/org/sample/SampleJava.java
+++ src/org/sample/SampleJava.java
@@ -2,4 +2,28 @@ package org.sample;
 
 public class SampleJava {
 
+	public void ef() {
+		System.out.println("ef");
+
+	}
+	
+	public void fg() {
+		System.out.println("fg");
+
+	}
+	
+	public void gh() {
+		System.out.println("gh");
+
+	}
+	
+	public void ij() {
+		System.out.println("ij");
+
+	}
+	
+	public void kl() {
+		System.out.println("kl");
+
+	}
 }
