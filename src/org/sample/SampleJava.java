0771649d413ece493ee4742ac6944b9ba02a0ec1
Sabasiva06 <sabasiva06@gmail.com>
2021-07-11 19:09:25 +0530

    A works

--- src/org/sample/SampleJava.java
+++ src/org/sample/SampleJava.java
@@ -2,4 +2,23 @@ package org.sample;
 
 public class SampleJava {
 
+	public void a() {
+		System.out.println("a");
+
+	}
+
+	public void b() {
+		System.out.println("b");
+
+	}
+
+	public void cd() {
+		System.out.println("C");
+
+	}
+
+	public void df() {
+		System.out.println("d");
+
+	}
 }
